17abe20fc49445d268475d6c9f67fc8438f08be8
FreedomAirways <116002276+FreeomAirways@users.noreply.github.com>
2023-02-20 14:14:35 +0000

    .

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import pygame, sys
 from pygame.locals import QUIT
 
+SIZE = 10
+WIDTH = 40
+HEIGHT = 30
+
 pygame.init()
-screen = pygame.display.set_mode((400, 300))
+size = (WIDTH * SIZE, HEIGHT * SIZE)
+screen = pygame.display.set_mode(size)
 background = (0, 0, 255)
 
-snake = [(20,20), (21, 20), (22, 20)]
+snake = [(20, 20), (21, 20), (22, 20)]
 
-SIZE = 10
 
 def game_to_screen(x, y):
     return x * SIZE, y * SIZE
+
 
 def draw():
     screen.fill(background)
@@ -18,15 +23,31 @@ def draw():
         x, y = part
         (screen_x, screen_y) = game_to_screen(x, y)
         color = (4, 235, 102)
-        rect = ((screen_x, screen_y), (SIZE, SIZE))  
+        rect = ((screen_x, screen_y), (SIZE, SIZE))
         pygame.draw.rect(screen, color, rect)
     pygame.display.update()
 
+
+step = (1, 0)
+
+
+def move():
+    x, y = snake[0]
+    dx, dy = step
+    new_x = (x + dx + WIDTH) % WIDTH
+    new_y = (y + dy + HEIGHT) % HEIGHT
+    snake.insert(0, (new_x, new_y))
+    snake.pop()
+
+
+clock = pygame.time.Clock()
+
 pygame.display.set_caption('Freedom Snake')
 while True:
-      draw()
-      for event in pygame.event.get():
+    move()
+    draw()
+    clock.tick(5)
+    for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-   
